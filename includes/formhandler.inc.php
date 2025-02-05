7a80db60c91ee80641d8ee4825f95e08dc1ed569
AadOfzo <aadofzo@gmail.com>
2025-02-05 13:09:16 +0100

    password aan users toegevoegd

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -2,18 +2,21 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
+    $pass = $_POST["pass"];
     $email = $_POST["email"];
     echo $username;
+    echo $pass;
     echo $email;
 
     try {
         require_once "dbh.inc.php";
 
-        $query = "INSERT INTO users (username, email) VALUES (:username, :email);";
+        $query = "INSERT INTO users (username, pass, email) VALUES (:username, :pass, :email);";
 
         $stmt = $pdo->prepare($query);
 
         $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pass", $pass);
         $stmt->bindParam(":email", $email);
 
         $stmt->execute();
--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 </head>
 
 <body>
-    <h1>Recepten</h1>
+
+    <h1>Sign up</h1>
     <form action="includes/formhandler.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="username" placeholder="Gebruikersnaam">
+        <input type="password" name="pass" placeholder="Paswoord">
         <input type="text" name="email" placeholder="E-Mail">
         <button>SignUp</button>
     </form>
